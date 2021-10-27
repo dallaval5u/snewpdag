15f3986b350ffac57a42f7c29f5ba0a1b3d43966
Riccardo Dallavalle <riccardo.dallavalle.001@student.uni.lu>
2021-10-27 13:09:51 +0100

    Writing a new graph which includes the summer plugins

--- snewpdag/data/test-liq-config.py
+++ snewpdag/data/test-liq-config.py
@@ -2,51 +2,85 @@
 # a test DAG for generating and analyzing two experiment alerts
 #
 
-var = [
+[
   {
     "name": "Control", "class": "Pass",
-    "kwargs": {"line": 100}
+    "kwargs": { "line": 100 }
   },
 
   {
-    "name": "JUNO-ts", "class": "gen.TimeSeries",
+    "name": "test_arr_time", "class": "gen.NeutrinoArrivalTime",
     "observe": ["Control"],
     "kwargs": {
-      "mean": 1000,
-      "sig_filetype": "tn", "sig_filename":
-        "snewpdag/data/output_scint20kt_27_Shen_1D_solar_mass_progenitor.fits_1msbin.txt"
+      "detector_list": ["JUNO","SK"],
+      "detector_location": "snewpdag/data/detector_location.csv"
     }
   },
 
-  {"name": "JUNO", "class": "gen.Combine", "observe": ["JUNO-ts"]},
+  {"name": "test_offset_time", "class":"gen.TimeOffset",
+   "observe": ["test_arr_time"],
+   "kwargs": {"detector_location": "snewpdag/data/detector_location.csv"}
+   },
+
+##### test time diff module from entry neutrino time
+
+  {"name": "test_det_time_SK", "class": "gen.DetectorTime",
+   "observe": ["test_offset_time"],
+   "kwargs": {"detector": "SK"}
+   },
+
+
+  {"name": "test_det_time_JUNO", "class": "gen.DetectorTime",
+   "observe": ["test_offset_time"],
+   "kwargs": {"detector": "JUNO"}
+   },
+
+  {"name": "test_nutime_extraction", "class": "gen.DeltaTCalculator",
+   "observe": ["test_det_time_JUNO","test_det_time_SK"]
+   },
+
+
+
+
+  {
+    "name": "JUNO-ts", "class": "gen.TimeSeries",
+    "observe": [ "Control" ],
+    "kwargs": {
+      "mean": 1000,
+      "sig_filetype": "tn", "sig_filename":
+      "snewpdag/data/output_scint20kt_27_Shen_1D_solar_mass_progenitor.fits_1msbin.txt"
+    }
+  },
+
+  { "name": "JUNO", "class": "gen.Combine", "observe": [ "JUNO-ts" ] },
 
   {
     "name": "JUNO-out", "class": "Pass",
-    "observe": ["JUNO"],
-    "kwargs": {"line": 1, "dump": 1}
+    "observe": [ "JUNO" ],
+    "kwargs": { "line": 1, "dump": 1 }
   },
 
   {
     "name": "JUNO-bin", "class": "BinnedAccumulator",
-    "observe": ["JUNO"],
+    "observe": [ "JUNO" ],
     "kwargs": {
       "in_field": "times",
       "nbins": 2000, "xlow": -10.0, "xhigh": 10.0,
       "out_xfield": "t", "out_yfield": "bins",
-      'flags': ['overflow'],
+      'flags': [ 'overflow' ],
     }
   },
 
   {
     "name": "JUNO-bin-out", "class": "Pass",
-    "observe": ["JUNO-bin"],
-    "kwargs": {"line": 1, "dump": 1}
+    "observe": [ "JUNO-bin" ],
+    "kwargs": { "line": 1, "dump": 1 }
   },
 
   {
     "name": "JUNO-bin-h",
     "class": "renderers.Histogram1D",
-    "observe": ["JUNO-bin"],
+    "observe": [ "JUNO-bin" ],
     "kwargs": {
       "title": "JUNO time profile",
       "xlabel": "time [s]",
@@ -57,42 +91,42 @@ var = [
 
   {
     "name": "SNOP-ts", "class": "gen.TimeSeries",
-    "observe": ["Control"],
+    "observe": [ "Control" ],
     "kwargs": {
       "mean": 100,
       "sig_filetype": "tn", "sig_filename":
-        "snewpdag/data/output_scint20kt_27_Shen_1D_solar_mass_progenitor.fits_1msbin.txt"
+      "snewpdag/data/output_scint20kt_27_Shen_1D_solar_mass_progenitor.fits_1msbin.txt"
     }
   },
 
-  {"name": "SNOP", "class": "gen.Combine", "observe": ["SNOP-ts"]},
+  { "name": "SNOP", "class": "gen.Combine", "observe": [ "SNOP-ts" ] },
 
   {
     "name": "SNOP-out", "class": "Pass",
-    "observe": ["SNOP"],
-    "kwargs": {"line": 1, "dump": 1}
+    "observe": [ "SNOP" ],
+    "kwargs": { "line": 1, "dump": 1 }
   },
 
   {
     "name": "SNOP-bin", "class": "BinnedAccumulator",
-    "observe": ["SNOP"],
+    "observe": [ "SNOP" ],
     "kwargs": {
       "in_field": "times",
       "nbins": 2000, "xlow": -10.0, "xhigh": 10.0,
       "out_xfield": "t", "out_yfield": "bins",
-      'flags': ['overflow'],
+      'flags': [ 'overflow' ],
     }
   },
 
   {
     "name": "SNOP-bin-out", "class": "Pass",
-    "observe": ["SNOP-bin"],
-    "kwargs": {"line": 1, "dump": 1}
+    "observe": [ "SNOP-bin" ],
+    "kwargs": { "line": 1, "dump": 1 }
   },
 
   {
     "name": "SNOP-bin-h", "class": "renderers.Histogram1D",
-    "observe": ["SNOP-bin"],
+    "observe": [ "SNOP-bin" ],
     "kwargs": {
       "title": "SNOP time profile",
       "xlabel": "time [s]",
@@ -103,19 +137,19 @@ var = [
 
   {
     "name": "Diff", "class": "NthTimeDiff",
-    "observe": ["JUNO", "SNOP"],
-    "kwargs": {"nth": 1}
+    "observe": [ "JUNO", "SNOP" ],
+    "kwargs": { "nth": 1 }
   },
 
   {
     "name": "Diff-out", "class": "Pass",
-    "observe": ["Diff"],
-    "kwargs": {"line": 100, "dump": 1}
+    "observe": [ "Diff" ],
+    "kwargs": { "line": 100, "dump": 1 }
   },
 
   {
     "name": "Diff-dt", "class": "Histogram1D",
-    "observe": ["Diff"],
+    "observe": [ "Diff" ],
     "kwargs": {
       "in_field": "dt",
       "nbins": 100, "xlow": -0.1, "xhigh": 0.1,
@@ -124,14 +158,14 @@ var = [
 
   {
     "name": "Diff-dt-out", "class": "Pass",
-    "observe": ["Diff-dt"],
-    "kwargs": {"line": 100, "dump": 1}
+    "observe": [ "Diff-dt" ],
+    "kwargs": { "line": 100, "dump": 1 }
   },
 
   {
     "name": "Diff-dt-h",
     "class": "renderers.Histogram1D",
-    "observe": ["Diff-dt"],
+    "observe": [ "Diff-dt" ],
     "kwargs": {
       "title": "Time difference",
       "xlabel": "dt [s]",
@@ -139,6 +173,16 @@ var = [
       "filename": "output/gen-liq-{}-{}-{}.png"
     }
   }
+    ,
+
+ #   {
+ #       "name": "test_ki2_calculator", "class": "Chi2Calculator",
+ #       "observe": ["JUNO", "SNOP"],
+ #       "kwargs": {
+ #           "detector_list": ["SNOP", "JUNO"],
+ #           "detector_location": "snewpdag/data/detector_location.csv",
+ #           "NSIDE": 32}
+ #   }
 
 ]
 
